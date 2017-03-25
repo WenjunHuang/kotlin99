939b3dbb64457ce7afb4a38da89221c5cdbaadff
WenjunHuang <wenjun.huang@gmail.com>
2017-03-25 18:53:18 +0800

    add new problem solutions

--- src/main/kotlin/org/kotlin99/P26.kt
+++ src/main/kotlin/org/kotlin99/P26.kt
@@ -8,14 +8,16 @@ import org.junit.Test
  * Created by Wenjun on 2017/2/22.
  */
 
-fun <T> combinations(count: Int, list: List<T>): List<List<T>> {
+fun <T> combinations(count: Int, list: List<T>): List<Set<T>> {
 
-    if (count == 0 || count > list.size)
+    if (count == 0)
+        return listOf(emptySet())
+    if (count > list.size)
         return emptyList()
 
     return list.mapIndexed { index, t ->
-        combinations(count - 1, list.drop(index + 1)).map { listOf(t) + it }
-    }.first()
+        combinations(count - 1, list.drop(index + 1)).map{it+ listOf(t)}
+    }.flatMap { it }
 }
 
 class P26Test {
@@ -23,25 +25,31 @@ class P26Test {
     fun twoElement() {
         assertThat(combinations(1, "ab".toList()),
                 equalTo(listOf(
-                        "a".toList(),
-                        "b".toList()
+                        "a".toSet(),
+                        "b".toSet()
                 )))
+    }
+
+    @Test
+    fun `combinations of 2 of 2 elements`() {
+        assertThat(combinations(2, "ab".toList()),
+                equalTo(listOf("ab".toSet())))
     }
 
     @Test
     fun `generate the combinations of K distinct objects chosen from the N elements of a list`() {
         assertThat(combinations(3, "abcde".toList()),
-                equalTo(listOf<List<Char>>(
-                        "cba".toList(),
-                        "dba".toList(),
-                        "eba".toList(),
-                        "dca".toList(),
-                        "eca".toList(),
-                        "eda".toList(),
-                        "dcb".toList(),
-                        "ecb".toList(),
-                        "edb".toList(),
-                        "edc".toList()
+                equalTo(listOf(
+                        "cba".toSet(),
+                        "dba".toSet(),
+                        "eba".toSet(),
+                        "dca".toSet(),
+                        "eca".toSet(),
+                        "eda".toSet(),
+                        "dcb".toSet(),
+                        "ecb".toSet(),
+                        "edb".toSet(),
+                        "edc".toSet()
                 )))
     }
 }
