7fc7b5812951d01b40322008c17ac8eaa43f008d
WenjunHuang <wenjun.huang80@gmail.com>
2017-03-31 17:22:06 +0800

    P14 pass

--- src/main/scala/scala99/P14.scala
+++ src/main/scala/scala99/P14.scala
@@ -1,12 +1,13 @@
 package scala99
 
-import org.scalatest.{FlatSpec, Matchers}
 
 /**
   * Created by rick on 3/31/2017.
   */
 object P14 {
-    def duplicate[T](list:List[T]): List[T] = Nil
+    def duplicate[T](list:List[T]): List[T] = {
+        list.map(List.fill(2)(_)).flatten
+    }
 }
 
 
