93522f42081196d27f0e45e4a78e5c878a634a62
WenjunHuang <wenjun.huang80@gmail.com>
2017-04-28 12:40:46 +0800

    add junit runner

--- src/test/scala/scala99/P18Spec.scala
+++ src/test/scala/scala99/P18Spec.scala
@@ -1,10 +1,13 @@
 package scala99
 
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
 import org.scalatest.{FlatSpec, Matchers}
 
 /**
   * Created by rick on 2017/4/28.
   */
+@RunWith(classOf[JUnitRunner])
 class P18Spec extends FlatSpec with Matchers {
     def fixture = new {
         val source = List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)
